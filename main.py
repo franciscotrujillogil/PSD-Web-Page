22affbab6eb7ef5705a1399216fc1138ae45e888
Francisco Trujillo <franciscotrujillogil@gmail.com>
2024-01-28 00:06:26 -0600

    5

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
+#End
+    
